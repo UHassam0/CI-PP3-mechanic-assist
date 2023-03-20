41117bcf2ebbededd3787f5d468130386785b403
UHassam0 <umarhassam786@gmail.com>
2023-03-20 16:26:32 +0000

    Set up name input and validation

--- run.py
+++ run.py
@@ -14,4 +14,31 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET_CARS = GSPREAD_CLIENT.open('Car Models')
 SHEET = GSPREAD_CLIENT.open('Mechanic-Customers')
 
-print(SHEET.worksheet('Customer-Information').get_all_values())
+def get_name():
+    """
+    Input function to get customer name
+    """
+    while True:
+     
+        name = input('Customer Name: ')
+        
+        if validate_name(name):
+            break
+            
+    return name
+
+def validate_name(name):
+    """
+    Validate name input to ensure it is at least 2 letters and not blank
+    """
+    try:
+        str(name)
+        if len(name.strip()) < 3:
+            print('Must input name at least 2 letters')
+            return False
+    except ValueError:
+        print('Must input name with regular characters. Check formatting')
+        return False
+    return True
+
+get_name()
