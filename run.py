fe80a0c433ab3ec45b9556f759728fa0e15f6f15
UHassam0 <umarhassam786@gmail.com>
2023-03-20 16:10:30 +0000

    Set up and test connection to spreadsheets

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
+
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -10,6 +11,7 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET_CARS = GSPREAD_CLIENT.open('Car Models')
 SHEET = GSPREAD_CLIENT.open('Mechanic-Customers')
 
 print(SHEET.worksheet('Customer-Information').get_all_values())
